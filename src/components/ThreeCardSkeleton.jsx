2db783587ccc52dc061d9bee7caaacd689b34e15
Yoursadik-nazrul <sadikull3@gmail.com>
2024-07-13 23:18:29 +0600

    contact us page creartion done with responsive

--- src/components/ThreeCardSkeleton.jsx
+++ src/components/ThreeCardSkeleton.jsx
@@ -25,13 +25,6 @@ export const ThreeCardSkeleton = () => {
         <SkeletonLine className="h-4 w-4/5" />
         <SkeletonLine className="h-10 w-2/5" />
       </Skeleton>
-      <Skeleton className="w-full space-y-2.5 xl:max-w-md">
-        <SkeletonLine className="h-52 w-full" />
-        <SkeletonLine className="h-4 w-full" />
-        <SkeletonLine className="h-4 w-3/5" />
-        <SkeletonLine className="h-4 w-4/5" />
-        <SkeletonLine className="h-10 w-2/5" />
-      </Skeleton>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,21 +8,23 @@ const Home = () => {
         <div>
             <CarouselComponent />
 
-            <div className="flex flex-col items-center justify-center gap-2 p-5 dark:text-white">
-                <h2 className="lg:text-titledex text-titlemob font-title">Need Volunteers</h2>
-                <p className="text-center lg:w-1/2 w-full">
-                    Discover opportunities to make a difference in our community. Whether you have a few hours or want a regular role, join us in creating positive change. Your time and effort matter!
-                </p>
-            </div>
-            <NeedVolunteers sortOption="descending" limit={6} />
-            <div className="flex items-center justify-center">
-                <Link
-                    to="/need-volunteers"
-                    type="button"
-                    className="hover:scale-105 px-5 py-2 bg-secondary rounded-lg hover:border-b-4 hover:border-r-4 border-primary text-white hover:text-black font-medium"
-                >
-                    View All Announcements
-                </Link>
+            <div className="p-5">
+                <div className="flex flex-col items-center justify-center gap-2 p-5 dark:text-white">
+                    <h2 className="lg:text-titledex text-titlemob font-title">Need Volunteers</h2>
+                    <p className="text-center lg:w-1/2 w-full">
+                        Discover opportunities to make a difference in our community. Whether you have a few hours or want a regular role, join us in creating positive change. Your time and effort matter!
+                    </p>
+                </div>
+                <NeedVolunteers sortOption="descending" limit={6} />
+                <div className="flex items-center justify-center">
+                    <Link
+                        to="/need-volunteers"
+                        type="button"
+                        className="hover:scale-105 px-5 py-2 bg-secondary rounded-lg hover:border-b-4 hover:border-r-4 border-primary text-white hover:text-black font-medium"
+                    >
+                        View All Announcements
+                    </Link>
+                </div>
             </div>
         </div>
     );
